144456e536509c85d270672344588dabb18aabab
Fernando Valani Cavalcante <valanife@gmail.com>
2021-06-30 04:25:34 -0300

    get sneakers implemented

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import connection from './database.js';
+import express from "express";
+import cors from "cors";
+import connection from "./database.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/list/sneakers", async (req, res) => {
+  try {
+    if (req.query.filter?.length === 0) {
+      const sneakers = await connection.query(`SELECT * FROM sneakers;`);
+    } else {
+      let nums = "";
+      if (typeof req.query.filter === "object") {
+        req.query.filter.forEach(
+          (item, i) => (nums = nums + ` OR ('%' || $${i + 2} || '%')`)
+        );
+      }
+      const sneakers = await connection.query(
+        `SELECT * FROM sneakers WHERE description ILIKE '(% || $1 || %)'${nums}`,
+        req.query.filter
+      );
+      res.send(sneakers.rows);
+    }
+    res.sendStatus(400);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
 
 export default app;

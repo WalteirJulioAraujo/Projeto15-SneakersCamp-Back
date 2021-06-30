92a1707c1af0d4fd11ec633e6e1b3e9de9f5db55
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-06-30 11:41:02 -0300

    Começando os testes

---
+++ tests/login.test.js
@@ -0,0 +1,9 @@
+import supertest from 'supertest';
+import app from '../src/app.js';
+import connection from '../src/database.js';
+
+describe('POST /login', ()=>{
+    it('returns status 200 for valid params', async ()=>{
+        
+    });
+});

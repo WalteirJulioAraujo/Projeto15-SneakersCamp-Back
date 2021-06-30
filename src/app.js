0ddba9571eaec1004366a97b719089f6313f1f13
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-06-30 10:16:07 -0300

    post login

--- src/app.js
+++ src/app.js
@@ -1,9 +1,57 @@
 import express from 'express';
 import cors from 'cors';
 import connection from './database.js';
+import { v4 as uuid } from 'uuid';
+import { LogInSchema } from './schemas/AllSchemas.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.post('/login', async (req,res)=>{
+    const { email, password } = req.body;
+
+    const validate = LogInSchema.validate(req.body);
+    if(validate.error){
+        console.log(validate.error);
+        return res.sendStatus(500);
+    }
+    try{
+        const searchEmail = await connection.query(`
+        SELECT * FROM users
+        WHERE email = $1
+        `,[email]);
+        const user = searchEmail.rows[0];
+
+        const searchIfAlreadyExistsSession = await connection.query(`
+        SELECT * FROM sessions
+        WHERE "userId" = $1
+        `,[user.id]);
+        if(searchIfAlreadyExistsSession.rows[0]){
+            await connection.query(`
+            DELETE FROM sessions
+            WHERE "userId" = $1
+            `,[user.id]);
+        }
+    
+        if(user && bcrypt.compareSync(password, user.password)){
+            const token = uuid();
+            await connection.query(`
+            INSERT INTO sessions
+            ("userId",token)
+            VALUES ($1,$2)
+            `,[user.id,token]);
+            res.send({ name: user.name, token }).status(200);
+        }else{
+            return res.sendStatus(401);
+        }
+
+    }catch(error){
+        console.log(error);
+        res.sendStatus(500);
+    }
+
+});
+
 
 export default app;
--- src/schemas/AllSchemas.js
+++ src/schemas/AllSchemas.js
@@ -1,2 +1,7 @@
 import joi from 'joi';
 //Colocar todos os schemas do joi aqui
+
+export const LogInSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(3).required()
+});
